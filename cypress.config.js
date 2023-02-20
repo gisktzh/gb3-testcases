56e7e2f5648aab8af8b3359916b9b8f30a4ed7b1
Dmytro Bykov <dmytro.bykov@ebp.ch>
2023-02-20 15:16:15 +0100

    added reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  reporter: 'mocha-junit-reporter',
+  reporterOptions: {
+    mochaFile: 'cypress/results/gb3-test-output-[hash].xml',
+    toConsole: true,
+    attachments: true
+  },
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
