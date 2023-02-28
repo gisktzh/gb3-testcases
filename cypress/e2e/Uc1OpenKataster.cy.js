e2fb19d9c7d31a23c74b205624c297907eb220e4
Dmytro Bykov <dmytro.bykov@ebp.ch>
2023-02-28 12:29:09 +0100

    test

--- cypress/e2e/Uc1OpenKataster.cy.js
+++ cypress/e2e/Uc1OpenKataster.cy.js
@@ -7,11 +7,12 @@ describe('template spec', () => {
 // Karte muss Liegenschaft markieren 
     //wait until request is fully loaded
     cy.get('map-page').click();
-    cy.get('span:contains("OerebKatasterZH")').should('be.visible').click(); 
+    cy.get('span:contains("OerebKatasterZH")').should('exist').click(); 
     // Highlights auswählen 
     cy.get('div:contains("ÖREB-Kataster (1 Treffer)") + b + button').click();
     //Pruefe Daten
     cy.get('div:contains("Fläche") + div:contains("1704")').should('be.visible');
     cy.get('button:contains("Drucken")').should('be.visible');
+
   })
 })
