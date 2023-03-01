303d7b750e258b7363fbcd0745f7e8ed455a4304
Dmytro Bykov <dmytro.bykov@ebp.ch>
2023-03-01 10:42:55 +0100

    tryxxx

--- cypress/e2e/Uc1OpenKataster.cy.js
+++ cypress/e2e/Uc1OpenKataster.cy.js
@@ -2,10 +2,16 @@ describe('template spec', () => {
   it('passes', () => {
     cy.open_url_with_cordinates();
     cy.select_topic("Raumplanung, Zonenpläne");
+
+    cy.intercept('GET', '**/OerebKatasterZH?**').as('kataster');
+    
     cy.click_map_in_the_list("ÖREB-Kataster");
+
+    cy.wait("@kataster").its('response.statusCode').should('eq', 200);
+
 //TODO Adresse suchen
 // Karte muss Liegenschaft markieren 
-    //wait until request is fully loaded
+    //wait until request is fully loaded 
    
 
     cy.intercept('GET', '**/feature_info?**').as('results');
@@ -16,13 +22,13 @@ describe('template spec', () => {
     cy.wait("@results").its('response.statusCode').should('eq', 200);
 
 
-   // cy.get('span:contains("OerebKatasterZH")').should('exist').and('be.visible').click();
+    cy.get('span:contains("OerebKatasterZH")').should('exist').and('be.visible').click();
 
-    // Highlights auswählen 
-   // cy.get('div:contains("ÖREB-Kataster (1 Treffer)") + b + button').click();
+     // Highlights auswählen 
+   cy.get('div:contains("ÖREB-Kataster (1 Treffer)") + b + button').click();
     //Pruefe Daten
-   // cy.get('div:contains("Fläche") + div:contains("1704")').should('be.visible');
-   // cy.get('button:contains("Drucken")').should('be.visible');
+    cy.get('div:contains("Fläche") + div:contains("1704")').should('be.visible');
+   cy.get('button:contains("Drucken")').should('be.visible');
 
   })
 })
