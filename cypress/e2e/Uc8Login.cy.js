4896163d411ede779ee1414351407ba6e01da220
Dmytro Bykov <dmytro.bykov@ebp.ch>
2023-03-03 15:59:43 +0100

    ts migration

--- cypress/e2e/Uc8Login.cy.js
+++
@@ -1,6 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://calm-plant-0ecbec603.2.azurestaticapps.net/maps?x=2702431&y=1241245&scale=3225&basemap=arelkbackgroundzh')
-    //To Do user meldet sich mit seinem Login und Passwort.
-  })
-})
