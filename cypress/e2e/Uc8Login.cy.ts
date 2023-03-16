76d3a1fd991391d9c5386da762c8d9357a17d745
Dmytro Bykov <dmytro.bykov@ebp.ch>
2023-03-16 16:52:55 +0100

    added login test, added secure placeholders

--- cypress/e2e/Uc8Login.cy.ts
+++ cypress/e2e/Uc8Login.cy.ts
@@ -1,9 +1,35 @@
+
 describe('template spec', () => {
   it('passes', () => {
     cy.open_url_with_cordinates('2682260', '1248390');
-    //cy.get('span:contains("Anmelden")').click();
-    //cy.get('input:contains("Benutzername")').type("test");
-    //cy.get('input:contains("Passwort")').type("test");
-    //assert
+    //assert map Betriebe is not visible
+    cy.select_topic('Bauten');
+
+    cy.get('span:contains("Betriebe")').should('not.be.exist');
+    
+    cy.get('span:contains("Login")').click();
+
+    
+  cy.origin('https://maps.zh.ch', () => {
+    const userName: string = Cypress.env('eIAM_username') as string;
+    const password: string = Cypress.env('eIAM_password') as string;
+  cy.get('#user_login').type(userName);
+  cy.get('#user_password').type(password);
+  cy.get('input[value="Login"]').click();
+  });
+
+  cy.get('span:contains("GIS-Browser")').last().click();
+  //assert request send authorization token
+  cy.intercept('GET', 'https://maps.zh.ch/v3/topics', (req) => {
+    expect(req.headers.authorization).to.include('Bearer')
+  }).as('topics');
+ 
+  cy.wait('@topics');
+ //assert map Betriebe is visible after log in
+  cy.select_topic('Bauten');
+  cy.get('span:contains("Betriebe")').should('be.visible').click();
+
+  cy.get('span:contains("GB3 User View")').should('be.visible').click();
+  cy.get('span:contains("Logout")').should('be.visible');
   });
 });
