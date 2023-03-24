ff5506ff4af5cf4373b6e215aaebfc5fff38bbba
Dmytro Bykov <dmytro.bykov@ebp.ch>
2023-03-24 09:17:28 +0100

    fix filter test

--- cypress/e2e/FilterLayers.cy.ts
+++ cypress/e2e/FilterLayers.cy.ts
@@ -8,13 +8,13 @@ describe('Filter layers', () => {
     cy.get('p:contains("Wasser")').should('not.exist');
     cy.select_topic('Raumplanung, Zonenpläne');
     //assert  ÖREB-Kataster layer is visible
-    cy.get('span:contains("ÖREB-Kataster") > button[data-test-id="add-active-map"]',{timeout:5000}).should('be.visible');
+    cy.get('span:contains("ÖREB-Kataster") > button[data-test-id="add-active-map"]').should('exist');
     //tre with other layer;
     cy.get('.map-catalogue__filter').clear().type('Verkehrstechnik (BSA)');
     //assert  Wasser topic is visible and others are not
     cy.get('p:contains("Raumplanung, Zonenpläne")').should('not.exist');
     cy.get('p:contains("Verkehr")').should('exist').click();
     //assert  layer is visible
-    cy.get('span:contains("Verkehrstechnik (BSA)") > button[data-test-id="add-active-map"]',{timeout:5000}).should('be.visible');
+    cy.get('span:contains("Verkehrstechnik (BSA)") > button[data-test-id="add-active-map"]').should('exist');
   });
 });
