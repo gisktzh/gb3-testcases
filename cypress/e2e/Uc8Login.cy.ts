0a2e1d90d4e2406394f7ca4c9ce95e75bede59ae
Dmytro Bykov <dmytro.bykov@ebp.ch>
2023-04-25 15:44:42 +0200

    test

--- cypress/e2e/Uc8Login.cy.ts
+++ cypress/e2e/Uc8Login.cy.ts
@@ -1,3 +1,10 @@
+beforeEach(() => {
+  cy.intercept('https://maps.zh.ch/v3/auth/authorize?**', (req) => {
+    req.url = 'https://maps.zh.ch/v3/auth/authorize?response_type=code&client_id=gb3&state=eE9MQlQyTmVXbDVwdFgyR2xjU0t6QzA2bm9teXdrNC1LOVJNLVg4ZEV1WHR2&redirect_uri=https%3A%2F%2Fcalm-plant-0ecbec603.2.azurestaticapps.net%2F&scope=openid%20profile&code_challenge=AoYNZ3YMXYS8-kfAEW6OykknvxueAlsi6r0fwePZy64&code_challenge_method=S256&nonce=eE9MQlQyTmVXbDVwdFgyR2xjU0t6QzA2bm9teXdrNC1LOVJNLVg4ZEV1WHR2&response_mode=query';
+    req.continue();
+  }).as('login');
+});
+
 describe('Login', () => {
   it('Login', () => {
     cy.open_url_with_cordinates('2682260', '1248390');
